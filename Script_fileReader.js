2f7df27aa77a39c5bacdf4bbdb2078e154334867
brocii <biroli.lucia@itiscannizzaro.edu.it>
2025-01-10 08:36:07 +0100

    Update Script_fileReader.js

--- Script_fileReader.js
+++ Script_fileReader.js
@@ -20,10 +20,12 @@ function inserisci(){
     for(let n = 0; n< righe.length; n++){
         tabella[n]=righe[n].split(",");
         let nuovaRiga = tab.insertRow();
-        let anno = nuovaRiga.insertCell(0);
-        anno.innerHTML = tabella[n][0].replace(/"/g,' ');
-        let numero = nuovaRiga.insertCell(1);
-            numero.innerHTML = tabella[n][1].replace(/"/g,' ');
+        for(let z=0; z<tabella.length; z++){
+            let cella = nuovaRiga.insertCell(0);
+            anno.innerHTML = tabella[n][z].replace(/"/g,' ');
+        
+        }
+        
            
     }   
 
