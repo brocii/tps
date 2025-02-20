d7878f4e3cd18bb8bc84533bd815e217ae707bf1
Lucia Biroli <biroli.lucia@itiscannizzaro.edu.it>
2025-02-20 18:20:12 +0100

    Add files via upload

--- script_a2.js
+++ script_a2.js
@@ -1,69 +1,54 @@
+let i = 0;
+let tabella = [];
 
-let i=0
+function salvaDati() {
+    const dati = {
+        nome: document.getElementById("nome").value,
+        cognome: document.getElementById("cognome").value,
+        citta: document.getElementById("citta").value,
+        indirizzo: document.getElementById("indirizzo").value,
+        mail: document.getElementById("mail").value
+    };
 
-function salvaDati(){
-    
-    let dati=[]
-    dati.push(nome=document.getElementById("nome").value);
-    dati.push(cognome=document.getElementById("cognome").value)
-    dati.push(citta=document.getElementById("citta").value);
-    dati.push(indirizzo=document.getElementById("indirizzo").value);
-    dati.push(mail=document.getElementById("mail").value);
+    tabella.push(dati);
+    localStorage.setItem("tabella", JSON.stringify(tabella));
 
-    
-    localStorage.setItem("utente"+i,dati)
-    localStorage.setItem("contatore",i)
-    i++;
-    document.getElementById("nome").value="";
-    document.getElementById("cognome").value="";
-    document.getElementById("citta").value="";
-    document.getElementById("indirizzo").value="";
-    document.getElementById("mail").value="";
-    dati=[];
+    document.getElementById("nome").value = "";
+    document.getElementById("cognome").value = "";
+    document.getElementById("citta").value = "";
+    document.getElementById("indirizzo").value = "";
+    document.getElementById("mail").value = "";
+
     inserisciDati();
-    
-    
 }
+
 function inserisciDati() {
-    let tab = document.getElementById("tabella")
+    let tab = document.getElementById("tabella");
     tab.innerHTML = "";  
-    let j=localStorage.getItem("contatore")
+    let t = JSON.parse(localStorage.getItem("tabella"));
 
-    
-    for (let z = 0; z <= j; z++) {
-        let utente = localStorage.getItem("utente" + z);
+    for (let z = 0; z < t.length; z++) {
+        let utente = t[z];
+
+        let nuovaRiga = tab.insertRow();
+        let contatore = nuovaRiga.insertCell(0);
+        contatore.innerHTML = z + 1;
+
+        let nome = nuovaRiga.insertCell(1);
+        nome.innerHTML = utente.nome;
         
-            
-            let info = utente.split(",");
+        let cognome = nuovaRiga.insertCell(2);
+        cognome.innerHTML = utente.cognome;
 
-            let nuovaRiga = tab.insertRow();
+        let citta = nuovaRiga.insertCell(3);
+        citta.innerHTML = utente.citta;
 
-            
-            let contatore = nuovaRiga.insertCell(0);
-            contatore.innerHTML = z+1;
+        let indirizzo = nuovaRiga.insertCell(4);
+        indirizzo.innerHTML = utente.indirizzo;
 
-            let nome = nuovaRiga.insertCell(1);
-            nome.innerHTML = info[0]; 
-
-            let cognome = nuovaRiga.insertCell(2);
-            cognome.innerHTML = info[1]; 
-
-            let citta = nuovaRiga.insertCell(3);
-            citta.innerHTML = info[2]; 
-
-            let indirizzo = nuovaRiga.insertCell(4);
-            indirizzo.innerHTML = info[3]; 
-
-            let mail = nuovaRiga.insertCell(5);
-            mail.innerHTML = info[4]; 
+        let mail = nuovaRiga.insertCell(5);
+        mail.innerHTML = utente.mail;
     }
-}  
-
-
-
-
-
-
-
+}
 
 
